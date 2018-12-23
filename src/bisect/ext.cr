de8b8a0918afda5f0ce33c16454be893ca9837b5
Stephen von Takach <steve@acaprojects.com>
2018-12-23 18:27:09 +1100

    extract extension methods into a helper module
    so it can be included into Array child classes

--- src/bisect/ext.cr
+++ src/bisect/ext.cr
@@ -1,27 +1,5 @@
-require "../bisect"
+require "./helper"
 
 class Array
-  def insort_left(x, lo = 0, hi = self.size)
-    Bisect.insort_left(self, x, lo, hi)
-  end
-
-  def insort_right(x, lo = 0, hi = self.size)
-    Bisect.insort_right(self, x, lo, hi)
-  end
-
-  def insort(x, lo = 0, hi = self.size)
-    Bisect.insort_right(self, x, lo, hi)
-  end
-
-  def bisect_left(x, lo = 0, hi = self.size)
-    Bisect.bisect_left(self, x, lo, hi)
-  end
-
-  def bisect_right(x, lo = 0, hi = self.size)
-    Bisect.bisect_right(self, x, lo, hi)
-  end
-
-  def bisect(x, lo = 0, hi = self.size)
-    Bisect.bisect_right(self, x, lo, hi)
-  end
+  include Bisect::Helper
 end
