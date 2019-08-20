0f32d55bbad6e83a328d43725de8fbc76bea1bba
baxter2 <baxcallan@gmail.com>
2019-08-21 02:41:53 +1000

    Utilize the existing methods to implement standard lookups for sorted arrays.

--- src/bisect/helper.cr
+++ src/bisect/helper.cr
@@ -24,4 +24,20 @@ module Bisect::Helper
   def bisect(x, lo = 0, hi = self.size)
     Bisect.bisect_right(self, x, lo, hi)
   end
+
+  def find_gt(x)   
+    Bisect.find_gt(self, x)
+  end
+
+  def find_ge(x)   
+    Bisect.find_ge(self, x)
+  end
+
+  def find_lt(x)   
+    Bisect.find_lt(self, x)
+  end
+
+  def find_le(x)   
+    Bisect.find_le(self, x)
+  end
 end
